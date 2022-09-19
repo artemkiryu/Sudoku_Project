6c67e7c57ae090b56736ce98a724f0ad2cb09c65
Artem Kiryukhin <66700598+artemkiryu@users.noreply.github.com>
2022-09-19 13:51:39 -0400

    Update sudoku_empty.py

--- sudoku_empty.py
+++ sudoku_empty.py
@@ -1,26 +1,16 @@
-############################################################
-# OPTIONAL HELPER FUNCTIONS
-############################################################
 
 def find_next_empty(puzzle):
     # finds the next row, col on puzzle that's not filled yet --> we represent these with -1
     # returns a row, col tuple (or (None, None) if there is none)
     pass
-
 def is_valid(puzzle, guess, row, col):
     # figures out whether the guess at the row/col of the puzzle is a valid guess
     # returns True or False
     pass
-
-############################################################
-# SOLVER IMPLEMENTATION
-############################################################
-
 def solve_sudoku(puzzle):
     # solve sudoku using backtracking!
     # our puzzle is a list of lists, where each inner list is a row in our sudoku puzzle
     # return solution
-
     # step 1: choose somewhere on the puzzle to make a guess
     # step 1.1: if there's nowhere left, then we're done because we only allowed valid inputs
     # step 2: if there is a place to put a number, then make a guess between 1 and 9
